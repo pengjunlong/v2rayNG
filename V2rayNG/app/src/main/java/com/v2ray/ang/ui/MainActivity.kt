e7ba695f9b6a061d536555c2725d1c7dc423950c
pengjunlong <junlong666@gmail.com>
2026-03-30 17:32:55 +0800

    选中第一个（延迟最低的节点）

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
@@ -94,7 +94,10 @@ class MainActivity : HelperBaseActivity(), NavigationView.OnNavigationItemSelect
         })
 
         binding.fab.setOnClickListener { handleFabAction() }
-        binding.fabLocate.setOnClickListener { updateSubscriptionThenTestAndSort()//locateSelectedServer() }
+        binding.fabLocate.setOnClickListener {
+            updateSubscriptionThenTestAndSort()
+            //locateSelectedServer()
+        }
         binding.layoutTest.setOnClickListener { handleLayoutTestClick() }
 
         setupGroupTab()
@@ -581,9 +584,13 @@ class MainActivity : HelperBaseActivity(), NavigationView.OnNavigationItemSelect
                         mainViewModel.sortByTestResults()
                         withContext(Dispatchers.Main) {
                             mainViewModel.reloadServerList()
+                            // Select the first server in the sorted list (fastest)
+                            val firstGuid = mainViewModel.serversCache.firstOrNull()?.guid
+                            if (firstGuid != null) {
+                                MmkvManager.setSelectServer(firstGuid)
+                            }
                             toast(R.string.toast_sort_done)
                             hideLoading()
-                            mainViewModel.selectFirstServerInList()
                         }
                     }
                 }
